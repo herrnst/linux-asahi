5328884df5bb602954ad6711e287555693cd0692
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2021-08-27 12:54:09 -0400

    iommu: io-pgtable: Add locked DART quirk
    Apple DARTs can be locked by the bootloader. In particular, we cannot
    change the TTBR address for these DARTs. However, we may take over the
    existing allocated page. We clear the existing page table. Important
    mappings will be restored by reserved regions.
    Add a quirk for this case and handle it.
    
    Signed-off-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
    Co-developed-by: Janne Grunau <j@jannau.net>
    Signed-off-by: Janne Grunau <j@jannau.net>

--- include/linux/io-pgtable.h
+++ include/linux/io-pgtable.h
@@ -85,6 +85,9 @@ struct io_pgtable_cfg {
 	 *
 	 * IO_PGTABLE_QUIRK_ARM_OUTER_WBWA: Override the outer-cacheability
 	 *	attributes set in the TCR for a non-coherent page-table walker.
+	 *
+	 * IO_PGTABLE_QUIRK_APPLE_LOCKED: Cannot modify the TTBR pointer, must
+	 *	inherit mappings from the bootloader.
 	 */
 	#define IO_PGTABLE_QUIRK_ARM_NS			BIT(0)
 	#define IO_PGTABLE_QUIRK_NO_PERMS		BIT(1)
@@ -92,6 +95,7 @@ struct io_pgtable_cfg {
 	#define IO_PGTABLE_QUIRK_ARM_MTK_TTBR_EXT	BIT(4)
 	#define IO_PGTABLE_QUIRK_ARM_TTBR1		BIT(5)
 	#define IO_PGTABLE_QUIRK_ARM_OUTER_WBWA		BIT(6)
+	#define IO_PGTABLE_QUIRK_APPLE_LOCKED		BIT(7)
 	unsigned long			quirks;
 	unsigned long			pgsize_bitmap;
 	unsigned int			ias;
