7299710ce39a96c49e11a9a49c6ad3f5061e9a54
Janne Grunau <j@jannau.net>
2021-12-12 20:40:04 +0100

    HID: add device IDs for Apple SPI HID devices
    Apple Silicon based laptop use SPI as transport for HID. Add support for
    SPI-based HID devices and and Apple keyboard and trackpad devices.
    Intel based laptops using the keyboard input driver applespi use the
    same HID over SPI protocol and can be supported later.
    
    This requires SPI keyboard/mouse HID types since Apple's intenal
    keyboards/trackpads use the same product id.
    
    Signed-off-by: Janne Grunau <j@jannau.net>

--- drivers/hid/hid-core.c
+++ drivers/hid/hid-core.c
@@ -2366,6 +2366,9 @@ int hid_connect(struct hid_device *hdev, unsigned int connect_mask)
 	case BUS_SDW:
 		bus = "SOUNDWIRE";
 		break;
+	case BUS_SPI:
+		bus = "SPI";
+		break;
 	case BUS_VIRTUAL:
 		bus = "VIRTUAL";
 		break;
