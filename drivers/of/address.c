9c79d8383c05f4f03edb13cde45e87b9e5d5e7db
Hector Martin <marcan@marcan.st>
2022-03-12 00:07:09 +0900

    of: Demote "Bad cell count" to debug message
    This happens on the SPMI bus... TODO: figure out what the right solution
    is here.
    
    Signed-off-by: Hector Martin <marcan@marcan.st>

--- drivers/of/address.c
+++ drivers/of/address.c
@@ -561,7 +561,7 @@ static u64 __of_translate_address(struct device_node *node,
 		pbus = of_match_bus(parent);
 		pbus->count_cells(dev, &pna, &pns);
 		if (!OF_CHECK_COUNTS(pna, pns)) {
-			pr_err("Bad cell count for %pOF\n", dev);
+			pr_debug("Bad cell count for %pOF\n", dev);
 			return OF_BAD_ADDR;
 		}
 
