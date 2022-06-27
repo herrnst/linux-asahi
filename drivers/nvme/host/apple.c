49b7a69a334feda96cdf451a781060539afb9962
Hector Martin <marcan@marcan.st>
2022-06-27 21:47:43 +0900

    apple-nvme: Release power domains when probe fails
    Signed-off-by: Hector Martin <marcan@marcan.st>

--- drivers/nvme/host/apple.c
+++ drivers/nvme/host/apple.c
@@ -1578,6 +1578,7 @@ static struct apple_nvme *apple_nvme_alloc(struct platform_device *pdev)
 
 	return anv;
 put_dev:
+	apple_nvme_detach_genpd(anv);
 	put_device(anv->dev);
 	return ERR_PTR(ret);
 }
@@ -1619,6 +1620,7 @@ out_uninit_ctrl:
 	nvme_uninit_ctrl(&anv->ctrl);
 out_put_ctrl:
 	nvme_put_ctrl(&anv->ctrl);
+	apple_nvme_detach_genpd(anv);
 	return ret;
 }
 
