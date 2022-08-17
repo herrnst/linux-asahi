bb032cd822669603a30a88481c56830aa2c09a32
Asahi Lina <lina@asahilina.net>
2022-08-18 02:07:35 +0900

    iommu/io-pgtable: Add Apple UAT variant format
    Apple Silicon SoCs (M1, M2, etc.) have a GPU with an ARM64 firmware
    coprocessor. The firmware and the GPU share page tables in the standard
    ARM64 format (the firmware literally sets the base as its TTBR0/1
    registers). TTBR0 covers the low half of the address space and is
    intended to be per-GPU-VM (GPU user mappings and kernel-managed
    buffers), while TTBR1 covers the upper half and is global (firmware
    code, data, management structures shared with the AP, and a few
    GPU-accessible data structures).
    
    In typical Apple fashion, the permissions are interpreted differently
    from traditional ARM PTEs. By default, firmware mappings use Apple SPRR
    permission remapping. The firmware only uses that for its own
    code/data/MMIO mappings, and those pages are not accessible by the GPU
    hardware. We never need to touch/manage these mappings, so this patch
    does not support them.
    
    When a specific bit is set in the PTEs, permissions switch to a
    different scheme which supports various combinations of firmware/GPU
    access. This is the mode intended to be used by AP GPU drivers, and what
    we implement here.
    
    The prot bits are interpreted as follows:
    
    - IOMMU_READ and IOMMU_WRITE have the usual meaning.
    
    - IOMMU_PRIV creates firmware-only mappings (no GPU access)
    - IOMMU_NOEXEC creates GPU-only structures (no FW access)
    - Otherwise structures are accessible by both GPU and FW
    
    - IOMMU_MMIO creates Device mappings for firmware
    - IOMMU_CACHE creates Normal-NC mappings for firmware (cache-coherent
      from the point of view of the AP, but slower)
    - Otherwise creates Normal mappings for firmware (this requires manual
      cache management on the firmware side, as it is not coherent with the
      SoC fabric)
    
    GPU-only mappings (textures/etc) are expected to use IOMMU_CACHE and are
    seemingly coherent with the CPU (or otherwise the firmware/GPU already
    issue the required cache management operations when correctly
    configured).
    
    There is a GPU-RO/FW-RW mode, but it is not currently implemented (it
    doesn't seem to be very useful for the driver). There seems to be no
    real noexec control (i.e. for shaders) on the GPU side. All of these
    mappings are implicitly noexec for the firmware.
    
    Drivers are expected to fully manage per-user (TTBR0) page tables, but
    ownership of shared kernel (TTBR1) page tables is shared between the
    firmware and the AP OS. We handle this by simply using a smaller IAS to
    drop down one level of page tables, so the driver can install a PTE in
    the top-level (firmware-initialized) page table directly and just add an
    offset to the VAs passed into the io_pgtable code. This avoids having to
    have any special handling for this here. The firmware-relevant data
    structures are small, so we do not expect to ever require more VA space
    than one top-level PTE covers (IAS=36 for the next level, 64 GiB).
    
    Only 16K page mode is supported. The coprocessor MMU supports huge pages
    as usual for ARM64, but the GPU MMU does not, so we do not enable them.
    
    Signed-off-by: Asahi Lina <lina@asahilina.net>

--- include/linux/io-pgtable.h
+++ include/linux/io-pgtable.h
@@ -19,6 +19,7 @@ enum io_pgtable_fmt {
 	AMD_IOMMU_V2,
 	APPLE_DART,
 	APPLE_DART2,
+	APPLE_UAT,
 	IO_PGTABLE_NUM_FMTS,
 };
 
@@ -145,6 +146,10 @@ struct io_pgtable_cfg {
 			u32 n_ttbrs;
 			u32 n_levels;
 		} apple_dart_cfg;
+
+		struct {
+			u64	ttbr;
+		} apple_uat_cfg;
 	};
 };
 
@@ -259,5 +264,6 @@ extern struct io_pgtable_init_fns io_pgtable_arm_mali_lpae_init_fns;
 extern struct io_pgtable_init_fns io_pgtable_amd_iommu_v1_init_fns;
 extern struct io_pgtable_init_fns io_pgtable_amd_iommu_v2_init_fns;
 extern struct io_pgtable_init_fns io_pgtable_apple_dart_init_fns;
+extern struct io_pgtable_init_fns io_pgtable_apple_uat_init_fns;
 
 #endif /* __IO_PGTABLE_H */
