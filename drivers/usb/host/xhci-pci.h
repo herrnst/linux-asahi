59381038f1f3368d7c61493daa45be5faec03462
Hector Martin <marcan@marcan.st>
2022-08-30 02:11:48 +0900

    xhci-pci: asmedia: Add a firmware loader for ASM2214a chips
    Apple ships ASM2214a ICs in some Apple Silicon hardware (notably, the
    2021 iMac and the 2022 Mac Studio) without a flash ROM, and relies on
    the OS to load firmware on startup. Add support for this to the generic
    xhci-pci driver.
    
    The loader code first checks the firmware version, and only attempts to
    load firmware if the version isn't the known ROM version.
    
    Since this arrangement only exists on Apple machines so far, and Apple
    are the only source of the (non-redistributable) firmware intended for
    use on these machines, the firmware is named asmedia/asm2214a-apple.bin.
    If this style of firmware loading ever becomes necessary on non-Apple
    machines, we should add a generic firmware name at the time (if it can
    be part of linux-firmware) or another vendor-specific firmware name.
    
    Signed-off-by: Hector Martin <marcan@marcan.st>

--- drivers/usb/host/xhci-pci.h
+++ drivers/usb/host/xhci-pci.h
@@ -7,4 +7,22 @@
 int xhci_pci_common_probe(struct pci_dev *dev, const struct pci_device_id *id);
 void xhci_pci_remove(struct pci_dev *dev);
 
+struct xhci_driver_data {
+	u64 quirks;
+	const char *firmware;
+};
+
+#if IS_ENABLED(CONFIG_USB_XHCI_PCI_ASMEDIA)
+int asmedia_xhci_check_request_fw(struct pci_dev *dev,
+				  const struct pci_device_id *id);
+
+#else
+static inline int asmedia_xhci_check_request_fw(struct pci_dev *dev,
+						const struct pci_device_id *id)
+{
+	return 0;
+}
+
+#endif
+
 #endif
