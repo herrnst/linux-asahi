650dd6681e147eec97cf7fbf497e3f46f245454b
Asahi Lina <lina@asahilina.net>
2022-10-22 00:10:30 +0900

    rust: of: Add OF node abstraction
    This abstraction enables Rust drivers to walk Device Tree nodes and
    query their properties.
    
    Signed-off-by: Asahi Lina <lina@asahilina.net>

--- rust/helpers/of.c
+++ rust/helpers/of.c
@@ -1,8 +1,29 @@
 // SPDX-License-Identifier: GPL-2.0
 
 #include <linux/of.h>
+#include <linux/of_device.h>
 
 bool rust_helper_is_of_node(const struct fwnode_handle *fwnode)
 {
 	return is_of_node(fwnode);
+}
+
+const struct of_device_id *rust_helper_of_match_device(
+		const struct of_device_id *matches, const struct device *dev)
+{
+			return of_match_device(matches, dev);
+}
+
+#ifdef CONFIG_OF
+bool rust_helper_of_node_is_root(const struct device_node *np)
+{
+	return of_node_is_root(np);
+}
+#endif
+
+struct device_node *rust_helper_of_parse_phandle(const struct device_node *np,
+               const char *phandle_name,
+               int index)
+{
+	return of_parse_phandle(np, phandle_name, index);
 }
