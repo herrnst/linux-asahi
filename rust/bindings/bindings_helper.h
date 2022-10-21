e58bdd89d6511e2a5879632ea26c217db0248c50
Asahi Lina <lina@asahilina.net>
2022-10-22 00:10:30 +0900

    rust: of: Add OF node abstraction
    This abstraction enables Rust drivers to walk Device Tree nodes and
    query their properties.
    
    Signed-off-by: Asahi Lina <lina@asahilina.net>

--- rust/bindings/bindings_helper.h
+++ rust/bindings/bindings_helper.h
@@ -64,6 +64,8 @@
 #include <linux/mdio.h>
 #include <linux/mm.h>
 #include <linux/miscdevice.h>
+#include <linux/of.h>
+#include <linux/of_address.h>
 #include <linux/of_device.h>
 #include <linux/pci.h>
 #include <linux/phy.h>
--- rust/kernel/device.rs
+++ rust/kernel/device.rs
@@ -6,6 +6,8 @@
 
 use crate::{
     bindings, fmt,
+    of,
+    prelude::*,
     sync::aref::ARef,
     types::{ForeignOwnable, NotThreadSafe, Opaque},
 };
@@ -279,6 +281,13 @@ impl<Ctx: DeviceContext> Device<Ctx> {
     pub unsafe fn from_raw<'a>(ptr: *mut bindings::device) -> &'a Self {
         // SAFETY: Guaranteed by the safety requirements of the function.
         unsafe { &*ptr.cast() }
+    }
+
+    /// Gets the OpenFirmware node attached to this device
+    pub fn of_node(&self) -> Option<of::Node> {
+        let ptr = self.0.get();
+        // SAFETY: This is safe as long as of_node is NULL or valid.
+        unsafe { of::Node::get_from_raw((*ptr).of_node) }
     }
 
     /// Prints an emergency-level message (level 0) prefixed with device information.
