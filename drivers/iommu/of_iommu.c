c99953a355f376de1c725f0d082bb4d2d1ccb1ff
Janne Grunau <j@jannau.net>
2022-10-22 12:24:54 +0200

    iommu: Parse translated reserved regions
    These regions are setup by the boot loader and require an iommu to
    translate arbitray physical to device VA mappings.
    
    Signed-off-by: Janne Grunau <j@jannau.net>

--- drivers/iommu/of_iommu.c
+++ drivers/iommu/of_iommu.c
@@ -180,9 +180,7 @@ iommu_resv_region_get_type(struct device *dev,
 	if (start == phys->start && end == phys->end)
 		return IOMMU_RESV_DIRECT;
 
-	dev_warn(dev, "treating non-direct mapping [%pr] -> [%pap-%pap] as reservation\n", phys,
-		 &start, &end);
-	return IOMMU_RESV_RESERVED;
+	return IOMMU_RESV_TRANSLATED;
 }
 
 /**
@@ -253,8 +251,13 @@ void of_iommu_get_resv_regions(struct device *dev, struct list_head *list)
 				}
 				type = iommu_resv_region_get_type(dev, &phys, iova, length);
 
-				region = iommu_alloc_resv_region(iova, length, prot, type,
+				if (type == IOMMU_RESV_TRANSLATED)
+					region = iommu_alloc_resv_region_tr(phys.start, iova, length, prot, type,
+								    GFP_KERNEL);
+				else
+					region = iommu_alloc_resv_region(iova, length, prot, type,
 								 GFP_KERNEL);
+
 				if (region)
 					list_add_tail(&region->list, list);
 			}
