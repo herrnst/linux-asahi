2566057a9ba20fc0f46567d3298a9666cb0f31bf
Asahi Lina <lina@asahilina.net>
2023-02-05 20:53:47 +0900

    rust: drm: Add Device and Driver abstractions
    Add the initial abstractions for DRM drivers and devices. These go
    together in one commit since they are fairly tightly coupled types.
    
    A few things have been stubbed out, to be implemented as further bits of
    the DRM subsystem are introduced.
    
    Signed-off-by: Asahi Lina <lina@asahilina.net>

--- rust/bindings/bindings_helper.h
+++ rust/bindings/bindings_helper.h
@@ -6,10 +6,13 @@
  * Sorted alphabetically.
  */
 
+#include <drm/drm_device.h>
+#include <drm/drm_drv.h>
 #include <drm/drm_ioctl.h>
 #include <linux/delay.h>
 #include <linux/device.h>
 #include <linux/dma-mapping.h>
+#include <linux/fs.h>
 #include <linux/io-pgtable.h>
 #include <linux/ktime.h>
 #include <linux/of.h>
