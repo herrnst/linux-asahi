160e0514e613ab0d9bbea417d66ee391bae577a2
Asahi Lina <lina@asahilina.net>
2023-02-05 21:12:31 +0900

    rust: drm: gem: Add GEM object abstraction
    The DRM GEM subsystem is the DRM memory management subsystem used by
    most modern drivers. Add a Rust abstraction to allow Rust DRM driver
    implementations to use it.
    
    Signed-off-by: Asahi Lina <lina@asahilina.net>

--- rust/helpers.c
+++ rust/helpers.c
@@ -20,6 +20,7 @@
  * Sorted alphabetically.
  */
 
+#include <drm/drm_gem.h>
 #include <kunit/test-bug.h>
 #include <linux/bug.h>
 #include <linux/build_bug.h>
@@ -477,6 +478,28 @@ void rust_helper_dma_resv_unlock(struct dma_resv *obj)
 	dma_resv_unlock(obj);
 }
 EXPORT_SYMBOL_GPL(rust_helper_dma_resv_unlock);
+
+#endif
+
+#ifdef CONFIG_DRM
+
+void rust_helper_drm_gem_object_get(struct drm_gem_object *obj)
+{
+	drm_gem_object_get(obj);
+}
+EXPORT_SYMBOL_GPL(rust_helper_drm_gem_object_get);
+
+void rust_helper_drm_gem_object_put(struct drm_gem_object *obj)
+{
+	drm_gem_object_put(obj);
+}
+EXPORT_SYMBOL_GPL(rust_helper_drm_gem_object_put);
+
+__u64 rust_helper_drm_vma_node_offset_addr(struct drm_vma_offset_node *node)
+{
+	return drm_vma_node_offset_addr(node);
+}
+EXPORT_SYMBOL_GPL(rust_helper_drm_vma_node_offset_addr);
 
 #endif
 
