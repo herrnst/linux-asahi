9797900e90c8cea29af75491bc464c866f939f23
Asahi Lina <lina@asahilina.net>
2023-02-11 17:08:36 +0900

    rust: drm: sched: Add GPU scheduler abstraction
    The GPU scheduler manages scheduling GPU jobs and dependencies between
    them. This Rust abstraction allows Rust DRM drivers to use this
    functionality.
    
    Signed-off-by: Asahi Lina <lina@asahilina.net>

--- rust/bindings/bindings_helper.h
+++ rust/bindings/bindings_helper.h
@@ -36,6 +36,7 @@
 #include <drm/drm_gem_shmem_helper.h>
 #include <drm/drm_ioctl.h>
 #include <drm/drm_syncobj.h>
+#include <drm/gpu_scheduler.h>
 #include <kunit/test.h>
 #include <linux/auxiliary_bus.h>
 #include <linux/bitmap.h>
