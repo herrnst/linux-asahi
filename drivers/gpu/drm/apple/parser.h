f469b3eb787b61f31ce1d51e38fb73235271ce02
Sven Peter <sven@svenpeter.dev>
2023-02-12 15:51:58 +0100

    drm: apple: DCP AFK/EPIC support
    Signed-off-by: Sven Peter <sven@svenpeter.dev>
    Co-developed-by: Martin Povišer <povik+lin@cutebit.org>
    Signed-off-by: Martin Povišer <povik+lin@cutebit.org>
    Co-developed-by: Janne Grunau <j@jannau.net>
    Signed-off-by: Janne Grunau <j@jannau.net>

--- drivers/gpu/drm/apple/parser.h
+++ drivers/gpu/drm/apple/parser.h
@@ -31,7 +31,8 @@ struct dcp_display_mode *enumerate_modes(struct dcp_parse_ctx *handle,
 					 int height_mm, unsigned notch_height);
 int parse_display_attributes(struct dcp_parse_ctx *handle, int *width_mm,
 			     int *height_mm);
-
+int parse_epic_service_init(struct dcp_parse_ctx *handle, const char **name,
+			    const char **class, s64 *unit);
 
 struct dcp_sound_format_mask {
 	u64 formats;			/* SNDRV_PCM_FMTBIT_* */
