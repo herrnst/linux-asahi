3d5fe7d7e684aef672bc662aa1494c543a477428
Martin Povišer <povik+lin@cutebit.org>
2023-02-13 14:56:24 +0100

    gpu: drm: apple: Set up client of AV endpoint
    Signed-off-by: Martin Povišer <povik+lin@cutebit.org>

---
+++ drivers/gpu/drm/apple/av.h
@@ -0,0 +1,9 @@
+#ifndef __AV_H__
+#define __AV_H__
+
+#include "parser.h"
+
+//int avep_audiosrv_startlink(struct apple_dcp *dcp, struct dcp_sound_cookie *cookie);
+//int avep_audiosrv_stoplink(struct apple_dcp *dcp);
+
+#endif /* __AV_H__ */
