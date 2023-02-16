ad8b44fce161c20200efa2fca3c9b46cd2ecf566
Asahi Lina <lina@asahilina.net>
2023-02-16 20:26:46 +0900

    *RFL import: kernel::platform
    Commit reference: 3dfc5ebff103

--- rust/kernel/lib.rs
+++ rust/kernel/lib.rs
@@ -13,6 +13,7 @@
 
 #![no_std]
 #![feature(allocator_api)]
+#![feature(associated_type_defaults)]
 #![feature(coerce_unsized)]
 #![feature(const_mut_refs)]
 #![feature(const_refs_to_cell)]
@@ -48,6 +49,7 @@ pub mod ioctl;
 pub mod kunit;
 pub mod module_param;
 pub mod of;
+pub mod platform;
 pub mod prelude;
 pub mod print;
 pub mod siphash;
