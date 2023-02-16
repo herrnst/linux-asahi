bc2ff95e25f3f5de370d70476aeea9a19e1ff73e
Asahi Lina <lina@asahilina.net>
2023-02-16 20:26:46 +0900

    *RFL import: kernel::platform
    Commit reference: 3dfc5ebff103

--- rust/helpers.c
+++ rust/helpers.c
@@ -29,6 +29,9 @@
 #include <linux/instruction_pointer.h>
 #include <linux/lockdep.h>
 #include <linux/mutex.h>
+#include <linux/of.h>
+#include <linux/of_device.h>
+#include <linux/platform_device.h>
 #include <linux/refcount.h>
 #include <linux/sched/signal.h>
 #include <linux/siphash.h>
@@ -372,6 +375,28 @@ void rust_helper_memcpy_fromio(void *to, const volatile void __iomem *from, long
 	memcpy_fromio(to, from, count);
 }
 EXPORT_SYMBOL_GPL(rust_helper_memcpy_fromio);
+
+void *
+rust_helper_platform_get_drvdata(const struct platform_device *pdev)
+{
+	return platform_get_drvdata(pdev);
+}
+EXPORT_SYMBOL_GPL(rust_helper_platform_get_drvdata);
+
+void
+rust_helper_platform_set_drvdata(struct platform_device *pdev,
+				 void *data)
+{
+	platform_set_drvdata(pdev, data);
+}
+EXPORT_SYMBOL_GPL(rust_helper_platform_set_drvdata);
+
+const struct of_device_id *rust_helper_of_match_device(
+		const struct of_device_id *matches, const struct device *dev)
+{
+	return of_match_device(matches, dev);
+}
+EXPORT_SYMBOL_GPL(rust_helper_of_match_device);
 
 /*
  * `bindgen` binds the C `size_t` type as the Rust `usize` type, so we can
