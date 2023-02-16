af380d5f7944ebe301dd7658000db06a1add2302
Asahi Lina <lina@asahilina.net>
2023-02-16 20:26:46 +0900

    *RFL import: kernel::platform
    Commit reference: 3dfc5ebff103

--- rust/bindings/bindings_helper.h
+++ rust/bindings/bindings_helper.h
@@ -16,6 +16,7 @@
 #include <linux/io-pgtable.h>
 #include <linux/ktime.h>
 #include <linux/lockdep.h>
+#include <linux/platform_device.h>
 #include <linux/refcount.h>
 #include <linux/siphash.h>
 #include <linux/sched.h>
