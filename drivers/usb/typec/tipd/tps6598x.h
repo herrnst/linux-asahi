1c7e28ab7a164a52328d11bed336109ab2d755e8
Sven Peter <sven@svenpeter.dev>
2023-02-19 16:40:37 +0100

    usb: typec: tipd: Support alternate modes & redo hotplug
    Needs to be split & rebased onto a tipd fork of cd321x support.
    
    Co-developed-by: Sven Peter <sven@svenpeter.dev>
    Signed-off-by: Hector Martin <marcan@marcan.st>

--- drivers/usb/typec/tipd/tps6598x.h
+++ drivers/usb/typec/tipd/tps6598x.h
@@ -12,6 +12,7 @@
 #include <linux/mutex_types.h>
 #include <linux/power_supply.h>
 #include <linux/usb/typec.h>
+#include <linux/usb/typec_mux.h>
 #include <linux/workqueue.h>
 
 #ifndef __TPS6598X_H__
@@ -202,6 +203,11 @@
 #define TPS_DATA_STATUS_FORCE_LSX	     BIT(23)
 #define TPS_DATA_STATUS_POWER_MISMATCH	     BIT(24)
 
+/* modified TPS_REG_DATA_STATUS bits for CD321x (and likely also TPS65987DDK) */
+#define CD321X_DATA_STATUS_HPD_IRQ	     BIT(14)
+#define CD321X_DATA_STATUS_HPD_LEVEL	     BIT(15)
+#define CD321X_DATA_STATUS_USB4_CONNECTION   BIT(23)
+
 #define TPS_DATA_STATUS_DP_PIN_ASSIGNMENT_MASK GENMASK(11, 10)
 #define TPS_DATA_STATUS_DP_PIN_ASSIGNMENT(x) \
 	TPS_FIELD_GET(TPS_DATA_STATUS_DP_PIN_ASSIGNMENT_MASK, (x))
@@ -281,16 +287,19 @@
 #define TPS_REG_INT_MASK2		0x17
 #define TPS_REG_INT_CLEAR1		0x18
 #define TPS_REG_INT_CLEAR2		0x19
-#define TPS_REG_SYSTEM_POWER_STATE	0x20
 #define TPS_REG_STATUS			0x1a
+#define TPS_REG_SYSTEM_POWER_STATE	0x20
+#define TPS_REG_USB4_STATUS		0x24
+#define TPS_REG_DP_SID_STATUS		0x58
 #define TPS_REG_SYSTEM_CONF		0x28
 #define TPS_REG_CTRL_CONF		0x29
 #define TPS_REG_BOOT_STATUS		0x2D
 #define TPS_REG_POWER_STATUS		0x3f
 #define TPS_REG_PD_STATUS		0x40
 #define TPS_REG_RX_IDENTITY_SOP		0x48
-#define TPS_REG_DP_SID_CONFIG		0x51
+#define TPS_REG_CF_VID_STATUS		0x5e
 #define TPS_REG_DP_SID_STATUS		0x58
+#define TPS_REG_INTEL_VID_STATUS	0x59
 #define TPS_REG_DATA_STATUS		0x5f
 #define TPS_REG_SLEEP_CONF		0x70
 
@@ -313,16 +322,59 @@ struct tps6598x_rx_identity_reg {
 	struct usb_pd_identity identity;
 } __packed;
 
+/* TPS_REG_USB4_STATUS */
+struct tps6598x_usb4_status_reg {
+	u8 mode_status;
+	__le32 eudo;
+	__le32 unknown;
+} __packed;
+
+/* TPS_REG_DP_SID_STATUS */
+struct tps6598x_dp_sid_status_reg {
+	u8 mode_status;
+	__le32 status_tx;
+	__le32 status_rx;
+	__le32 configure;
+	__le32 mode_data;
+} __packed;
+
+/* TPS_REG_INTEL_VID_STATUS */
+struct tps6598x_intel_vid_status_reg {
+	u8 mode_status;
+	__le32 attention_vdo;
+	__le16 enter_vdo;
+	__le16 device_mode;
+	__le16 cable_mode;
+} __packed;
+
 struct tps6598x;
 
 struct tipd_data {
 	irq_handler_t irq_handler;
 	int (*register_port)(struct tps6598x *tps, struct fwnode_handle *node);
+	void (*unregister_port)(struct tps6598x *tps);
+	bool (*read_data_status)(struct tps6598x *tps);
 	void (*trace_power_status)(u16 status);
+	void (*trace_data_status)(u32 status);
 	void (*trace_status)(u32 status);
 	int (*apply_patch)(struct tps6598x *tps);
 	int (*init)(struct tps6598x *tps);
 	int (*reset)(struct tps6598x *tps);
+	void (*typec_update_mode)(struct tps6598x *tps);
+	void (*update_work)(struct work_struct *);
+};
+
+struct tps6598x_status {
+	u32 status;
+	u32 pwr_status;
+	u32 data_status;
+	u32 status_changed;
+	u32 pwr_status_changed;
+	u32 data_status_changed;
+	struct usb_pd_identity partner_identity;
+	struct tps6598x_dp_sid_status_reg dp_sid_status;
+	struct tps6598x_intel_vid_status_reg intel_vid_status;
+	struct tps6598x_usb4_status_reg usb4_status;
 };
 
 struct tps6598x {
@@ -334,6 +386,7 @@ struct tps6598x {
 
 	struct gpio_desc *reset;
 	struct typec_port *port;
+	struct fwnode_handle *connector_fwnode;
 	struct typec_partner *partner;
 	struct usb_pd_identity partner_identity;
 	struct usb_role_switch *role_sw;
@@ -346,9 +399,24 @@ struct tps6598x {
 	int wakeup;
 	u32 status; /* status reg */
 	u16 pwr_status;
+	u32 data_status;
+	struct tps6598x_dp_sid_status_reg dp_sid_status;
+	struct tps6598x_intel_vid_status_reg intel_vid_status;
+	struct tps6598x_usb4_status_reg usb4_status;
 	struct delayed_work	wq_poll;
 
 	const struct tipd_data *data;
+
+	/* CD321x-specific */
+	struct typec_altmode *port_altmode_dp;
+	struct typec_altmode *port_altmode_tbt;
+	struct typec_mux *mux;
+	struct typec_mux_state state;
+	struct mutex update_lock;
+	struct tps6598x_status update_status;
+	struct delayed_work update_work;
+	bool cur_partner_pd;
+	struct usb_pd_identity cur_partner_identity;
 };
 
 #endif /* __TPS6598X_H__ */
