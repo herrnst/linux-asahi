3faa35c07ea3499ad1794b580231aacf79cb2555
Asahi Lina <lina@asahilina.net>
2023-04-19 23:48:03 +0900

    drm/asahi: fw.workqueue: Barrier update for G14X
    Signed-off-by: Asahi Lina <lina@asahilina.net>

--- drivers/gpu/drm/asahi/queue/mod.rs
+++ drivers/gpu/drm/asahi/queue/mod.rs
@@ -634,6 +634,7 @@ impl Queue for Queue::ver {
                                     stamp_self: queue_job.event_info().value.next(),
                                     uuid: 0xffffbbbb,
                                     barrier_type: 0,
+                                    padding: Default::default(),
                                 }
                             ))
                         },
