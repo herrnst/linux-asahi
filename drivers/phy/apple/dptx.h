1114ddcb50934a6a4211dd7343dd63e85d8a9b10
Janne Grunau <j@jannau.net>
2023-05-05 17:40:26 +0200

    phy: apple: Add DP TX phy driver
    This driver is found on Apple's Mac mini (M2, 2023) and controls one
    output of the main display controller. It is connected to a MCDP 29XX
    (public known part is MCDP 2900) DP 1.4 to HDMI 2.0a protocol converter.
    
    Signed-off-by: Janne Grunau <j@jannau.net>

---
+++ drivers/phy/apple/dptx.h
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-2.0 OR BSD-2-Clause
+/*
+ * Apple DP TX PHY driver
+ *
+ * Copyright (C) The Asahi Linux Contributors
+ * Author: Janne Grunau <j@jannau.net>
+ */
+
+#ifndef PHY_APPLE_DPTX_H
+#define PHY_APPLE_DPTX_H
+
+enum dptx_phy_link_rate {
+	DPTX_PHY_LINK_RATE_RBR,
+	DPTX_PHY_LINK_RATE_HBR,
+	DPTX_PHY_LINK_RATE_HBR2,
+	DPTX_PHY_LINK_RATE_HBR3,
+};
+#endif /* PHY_APPLE_DPTX_H */
