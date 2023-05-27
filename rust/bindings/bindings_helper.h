a44e03be892fc3ac34e41ce9cbae249bd613e264
Asahi Lina <lina@asahilina.net>
2023-05-27 20:17:33 +0900

    rust: kernel: Add simple siphash abstraction
    This allows Rust code to use the Hasher interface with the kernel
    siphash implementation.
    
    Signed-off-by: Asahi Lina <lina@asahilina.net>

--- rust/bindings/bindings_helper.h
+++ rust/bindings/bindings_helper.h
@@ -13,6 +13,7 @@
 #include <linux/mdio.h>
 #include <linux/phy.h>
 #include <linux/refcount.h>
+#include <linux/siphash.h>
 #include <linux/sched.h>
 #include <linux/slab.h>
 #include <linux/wait.h>
