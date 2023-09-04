22ad228d778c4b01ad994c6688cc4e9854f86fbb
Janne Grunau <j@jannau.net>
2023-09-04 23:07:45 +0200

    drm: apple: Update supported firmware versions to 12.3 and 13.5
    Removes support for all firmware versions which report as compatible to
    13.3 except 13.5. This will be removed after m1n1 reports firmware 13.5
    as "apple,firmware-compat" for a while.
    The files with "v13_3" will be renamed at a later point to avoid
    conflicts with development trees.
    
    Signed-off-by: Janne Grunau <j@jannau.net>

--- drivers/gpu/drm/apple/iomfb.c
+++ drivers/gpu/drm/apple/iomfb.c
@@ -223,7 +223,7 @@ void dcp_sleep(struct apple_dcp *dcp)
 	case DCP_FIRMWARE_V_12_3:
 		iomfb_sleep_v12_3(dcp);
 		break;
-	case DCP_FIRMWARE_V_13_3:
+	case DCP_FIRMWARE_V_13_5:
 		iomfb_sleep_v13_3(dcp);
 		break;
 	default:
@@ -240,7 +240,7 @@ void dcp_poweron(struct platform_device *pdev)
 	case DCP_FIRMWARE_V_12_3:
 		iomfb_poweron_v12_3(dcp);
 		break;
-	case DCP_FIRMWARE_V_13_3:
+	case DCP_FIRMWARE_V_13_5:
 		iomfb_poweron_v13_3(dcp);
 		break;
 	default:
@@ -258,7 +258,7 @@ void dcp_poweroff(struct platform_device *pdev)
 	case DCP_FIRMWARE_V_12_3:
 		iomfb_poweroff_v12_3(dcp);
 		break;
-	case DCP_FIRMWARE_V_13_3:
+	case DCP_FIRMWARE_V_13_5:
 		iomfb_poweroff_v13_3(dcp);
 		break;
 	default:
@@ -503,7 +503,7 @@ void dcp_flush(struct drm_crtc *crtc, struct drm_atomic_state *state)
 	case DCP_FIRMWARE_V_12_3:
 		iomfb_flush_v12_3(dcp, crtc, state);
 		break;
-	case DCP_FIRMWARE_V_13_3:
+	case DCP_FIRMWARE_V_13_5:
 		iomfb_flush_v13_3(dcp, crtc, state);
 		break;
 	default:
@@ -519,7 +519,7 @@ void iomfb_start(struct apple_dcp *dcp)
 	case DCP_FIRMWARE_V_12_3:
 		iomfb_start_v12_3(dcp);
 		break;
-	case DCP_FIRMWARE_V_13_3:
+	case DCP_FIRMWARE_V_13_5:
 		iomfb_start_v13_3(dcp);
 		break;
 	default:
@@ -572,7 +572,7 @@ void iomfb_shutdown(struct apple_dcp *dcp)
 	case DCP_FIRMWARE_V_12_3:
 		iomfb_shutdown_v12_3(dcp);
 		break;
-	case DCP_FIRMWARE_V_13_3:
+	case DCP_FIRMWARE_V_13_5:
 		iomfb_shutdown_v13_3(dcp);
 		break;
 	default:
