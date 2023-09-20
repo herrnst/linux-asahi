5529f404e877493600bca3b8f1f47fdec638d2bf
Hector Martin <marcan@marcan.st>
2023-09-20 22:16:41 +0900

    fixup! drm/scheduler: Clean up jobs when the scheduler is torn down.

--- drivers/gpu/drm/scheduler/sched_main.c
+++ drivers/gpu/drm/scheduler/sched_main.c
@@ -1157,8 +1157,7 @@ void drm_sched_fini(struct drm_gpu_scheduler *sched)
 		list_del_init(&s_job->list);
 		spin_unlock(&sched->job_list_lock);
 
-		dma_fence_set_error(&s_job->s_fence->finished, -ESRCH);
-		drm_sched_fence_finished(s_job->s_fence);
+		drm_sched_fence_finished(s_job->s_fence, -ESRCH);
 
 		WARN_ON(s_job->s_fence->parent);
 		sched->ops->free_job(s_job);
