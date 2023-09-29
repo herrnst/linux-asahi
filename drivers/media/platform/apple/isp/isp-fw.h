11872483b4bf39bbc7156a9040e97502916dd123
Asahi Lina <lina@asahilina.net>
2023-09-29 18:38:22 +0900

    media: apple: isp: Remove ioread/iowrite and stop doing raw address translation
    Translating IOVAs via the DART and then trying to access physical memory
    directly is slow and error-prone. We know what surfaces IOVAs are
    supposed to be part of, so we can use the surface vmap to access the
    contents. Where we get an IOVA from the firmware, assert that it is
    within the expected range before accessing it.
    
    Since we're using threaded IRQs now, this also lets us get rid of the
    deferred vmap.
    
    Signed-off-by: Asahi Lina <lina@asahilina.net>

--- drivers/media/platform/apple/isp/isp-fw.h
+++ drivers/media/platform/apple/isp/isp-fw.h
@@ -12,4 +12,13 @@ void apple_isp_free_firmware_surface(struct apple_isp *isp);
 int apple_isp_firmware_boot(struct apple_isp *isp);
 void apple_isp_firmware_shutdown(struct apple_isp *isp);
 
+void *apple_isp_translate(struct apple_isp *isp, struct isp_surf *surf,
+			  dma_addr_t iova, size_t size);
+
+static inline void *apple_isp_ipc_translate(struct apple_isp *isp,
+					    dma_addr_t iova, size_t size)
+{
+	return apple_isp_translate(isp, isp->ipc_surf, iova, size);
+}
+
 #endif /* __ISP_FW_H__ */
