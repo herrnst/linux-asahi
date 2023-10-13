41d88b9be35041a3835cd5d6dbf186282e7fcc62
Eileen Yoon <eyn@gmx.com>
2023-10-13 21:09:43 +0900

    media: apple: isp: Support system sleep
    Signed-off-by: Eileen Yoon <eyn@gmx.com>

--- drivers/media/platform/apple/isp/isp-v4l2.h
+++ drivers/media/platform/apple/isp/isp-v4l2.h
@@ -10,4 +10,7 @@ int apple_isp_setup_video(struct apple_isp *isp);
 void apple_isp_remove_video(struct apple_isp *isp);
 int ipc_bt_handle(struct apple_isp *isp, struct isp_channel *chan);
 
+int apple_isp_video_suspend(struct apple_isp *isp);
+int apple_isp_video_resume(struct apple_isp *isp);
+
 #endif /* __ISP_V4L2_H__ */
