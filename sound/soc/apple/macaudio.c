39b843ac69111a7b796cc85d19e47583d8329028
Hector Martin <marcan@marcan.st>
2023-12-15 20:40:53 +0900

    READ COMMIT MESSAGE! macaudio: Enable second round of models
    Enables j316, j413, j415, j416
    
    *** WARNING FOR DISTRO PACKAGERS WANTING TO APPLY THIS: ***
    *** YOU ABSOLUTELY NEED THIS PATCH IN YOUR LSP-PLUGINS PACKAGE ***
    
    https://github.com/lsp-plugins/lsp-dsp-lib/pull/20
    
    Do NOT enable speakers without that patch, on any model. It can/will
    result in nasty noise that could damage them.
    
    Signed-off-by: Hector Martin <marcan@marcan.st>

--- sound/soc/apple/macaudio.c
+++ sound/soc/apple/macaudio.c
@@ -1504,7 +1504,7 @@ struct macaudio_platform_cfg macaudio_j314_cfg = {
 };
 
 struct macaudio_platform_cfg macaudio_j316_cfg = {
-	false,	AMP_SN012776,	SPKR_2W1T,	true,	15,	-20,
+	true,	AMP_SN012776,	SPKR_2W1T,	true,	15,	-20,
 };
 
 struct macaudio_platform_cfg macaudio_j37x_j47x_cfg = {
@@ -1512,11 +1512,11 @@ struct macaudio_platform_cfg macaudio_j37x_j47x_cfg = {
 };
 
 struct macaudio_platform_cfg macaudio_j413_cfg = {
-	false,	AMP_SN012776,	SPKR_1W1T,	true,	15,	-20,
+	true,	AMP_SN012776,	SPKR_1W1T,	true,	15,	-20,
 };
 
 struct macaudio_platform_cfg macaudio_j415_cfg = {
-	false,	AMP_SN012776,	SPKR_2W1T,	true,	15,	-20,
+	true,	AMP_SN012776,	SPKR_2W1T,	true,	15,	-20,
 };
 
 struct macaudio_platform_cfg macaudio_j45x_cfg = {
