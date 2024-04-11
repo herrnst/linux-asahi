23e15b395d49a5703a51f60d115bdba91f525080
Hector Martin <marcan@marcan.st>
2024-04-11 09:51:23 +0900

    arm64: Implement Apple IMPDEF TSO memory model control
    Apple CPUs may implement the TSO memory model as an optional
    configurable mode. This allows x86 emulators to simplify their
    load/store handling, greatly increasing performance.
    
    Expose this via the prctl PR_SET_MEM_MODEL_TSO mechanism. We use the
    Apple IMPDEF AIDR_EL1 register to check for the availability of TSO
    mode, and enable this codepath on all CPUs with an Apple implementer.
    
    This relies on the ACTLR_EL1 thread state scaffolding introduced
    earlier.
    
    Signed-off-by: Hector Martin <marcan@marcan.st>
    Reviewed-by: Neal Gompa <neal@gompa.dev>

--- arch/arm64/include/asm/cpufeature.h
+++ arch/arm64/include/asm/cpufeature.h
@@ -917,7 +917,8 @@ static inline unsigned int get_vmid_bits(u64 mmfr1)
 
 static __always_inline bool system_has_actlr_state(void)
 {
-	return false;
+	return IS_ENABLED(CONFIG_ARM64_ACTLR_STATE) &&
+		alternative_has_cap_unlikely(ARM64_HAS_TSO_APPLE);
 }
 
 s64 arm64_ftr_safe_value(const struct arm64_ftr_bits *ftrp, s64 new, s64 cur);
