e0d7f0a8dc08232a9f8c66cbf1e2784a387beec5
Hector Martin <marcan@marcan.st>
2024-04-11 09:51:22 +0900

    arm64: Introduce scaffolding to add ACTLR_EL1 to thread state
    Some CPUs expose IMPDEF features in ACTLR_EL1 that can be meaningfully
    controlled per-thread (like TSO control on Apple cores). Add the basic
    scaffolding to save/restore this register as part of context switching.
    
    This mechanism is disabled by default both by config symbol and via a
    runtime check, which ensures it is never triggered unless the system is
    known to need it for some feature (which also implies that the layout of
    ACTLR_EL1 is uniform between all CPU core types).
    
    Signed-off-by: Hector Martin <marcan@marcan.st>
    Reviewed-by: Neal Gompa <neal@gompa.dev>

--- arch/arm64/kernel/process.c
+++ arch/arm64/kernel/process.c
@@ -385,6 +385,11 @@ int copy_thread(struct task_struct *p, const struct kernel_clone_args *args)
 		if (system_supports_poe())
 			p->thread.por_el0 = read_sysreg_s(SYS_POR_EL0);
 
+#ifdef CONFIG_ARM64_ACTLR_STATE
+		if (system_has_actlr_state())
+			p->thread.actlr = read_sysreg(actlr_el1);
+#endif
+
 		if (stack_start) {
 			if (is_compat_thread(task_thread_info(p)))
 				childregs->compat_sp = stack_start;
@@ -585,6 +590,25 @@ int arch_prctl_mem_model_set(struct task_struct *t, unsigned long val)
 }
 #endif
 
+#ifdef CONFIG_ARM64_ACTLR_STATE
+/*
+ * IMPDEF control register ACTLR_EL1 handling. Some CPUs use this to
+ * expose features that can be controlled by userspace.
+ */
+static void actlr_thread_switch(struct task_struct *next)
+{
+	if (!system_has_actlr_state())
+		return;
+
+	current->thread.actlr = read_sysreg(actlr_el1);
+	write_sysreg(next->thread.actlr, actlr_el1);
+}
+#else
+static inline void actlr_thread_switch(struct task_struct *next)
+{
+}
+#endif
+
 /*
  * Thread switching.
  */
@@ -603,6 +627,7 @@ struct task_struct *__switch_to(struct task_struct *prev,
 	cntkctl_thread_switch(prev, next);
 	ptrauth_thread_switch_user(next);
 	permission_overlay_switch(next);
+	actlr_thread_switch(next);
 
 	/*
 	 * Complete any pending TLB or cache maintenance on this CPU in case
