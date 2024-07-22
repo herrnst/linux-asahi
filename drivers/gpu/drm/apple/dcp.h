8e23a1ecec24d0f01c831714e9993f269519a62a
Janne Grunau <j@jannau.net>
2024-07-22 20:26:55 +0200

    drm: apple: Add CRTC CRC support
    The DCP firmware has CRC support. While this is not yet reverse
    engineering report always 0 to at least be able to run tests from
    igt-gpu-tools with "--skip-crc-compare".
    
    Signed-off-by: Janne Grunau <j@jannau.net>

--- drivers/gpu/drm/apple/dcp.h
+++ drivers/gpu/drm/apple/dcp.h
@@ -31,6 +31,7 @@ struct apple_encoder {
 
 void dcp_poweroff(struct platform_device *pdev);
 void dcp_poweron(struct platform_device *pdev);
+int dcp_set_crc(struct drm_crtc *crtc, bool enabled);
 int dcp_crtc_atomic_check(struct drm_crtc *crtc, struct drm_atomic_state *state);
 int dcp_get_connector_type(struct platform_device *pdev);
 void dcp_link(struct platform_device *pdev, struct apple_crtc *apple,
