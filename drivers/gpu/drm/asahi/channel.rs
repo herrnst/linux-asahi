de9e7e329c77824899ad9ab36edfa0d8749a6521
Asahi Lina <lina@asahilina.net>
2024-09-24 03:28:40 +0900

    drm/asahi: gpu: Show unknown field in timeouts
    Signed-off-by: Asahi Lina <lina@asahilina.net>

--- drivers/gpu/drm/asahi/channel.rs
+++ drivers/gpu/drm/asahi/channel.rs
@@ -356,10 +356,10 @@ impl EventChannel::ver {
                         },
                         EventMsg::Timeout {
                             counter,
+                            unk_8,
                             event_slot,
-                            ..
                         } => match self.gpu.as_ref() {
-                            Some(gpu) => gpu.handle_timeout(counter, event_slot),
+                            Some(gpu) => gpu.handle_timeout(counter, event_slot, unk_8),
                             None => {
                                 dev_crit!(
                                     self.dev.as_ref(),
@@ -380,7 +380,6 @@ impl EventChannel::ver {
                             vm_slot,
                             buffer_slot,
                             counter,
-                            ..
                         } => match self.gpu.as_ref() {
                             Some(gpu) => {
                                 self.buf_mgr.grow(buffer_slot);
