f3d102675ddd546b5a7302322f28d69c63b2dc5f
Danilo Krummrich <dakr@kernel.org>
2024-10-15 16:04:37 +0200

    rust: pass module name to `Module::init`
    In a subsequent patch we introduce the `Registration` abstraction used
    to register driver structures. Some subsystems require the module name on
    driver registration (e.g. PCI in __pci_register_driver()), hence pass
    the module name to `Module::init`.
    
    Signed-off-by: Danilo Krummrich <dakr@kernel.org>

--- rust/kernel/net/phy.rs
+++ rust/kernel/net/phy.rs
@@ -899,7 +899,7 @@ macro_rules! module_phy_driver {
                 [$($crate::net::phy::create_phy_driver::<$driver>()),+];
 
             impl $crate::Module for Module {
-                fn init(module: &'static ThisModule) -> Result<Self> {
+                fn init(_name: &'static CStr, module: &'static ThisModule) -> Result<Self> {
                     // SAFETY: The anonymous constant guarantees that nobody else can access
                     // the `DRIVERS` static. The array is used only in the C side.
                     let drivers = unsafe { &mut DRIVERS };
