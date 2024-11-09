f3a724eee48eba977976de1906f90e09d7f32a4b
Sasha Finkelstein <fnkl.kernel@gmail.com>
2024-11-09 18:27:59 +0100

    rust: device: WIP(?): Make as_raw() public for AOP series
    Signed-off-by: Sasha Finkelstein <fnkl.kernel@gmail.com>
    Signed-off-by: Janne Grunau <j@jannau.net>

--- rust/kernel/device.rs
+++ rust/kernel/device.rs
@@ -247,7 +247,7 @@ impl Device<CoreInternal> {
 
 impl<Ctx: DeviceContext> Device<Ctx> {
     /// Obtain the raw `struct device *`.
-    pub(crate) fn as_raw(&self) -> *mut bindings::device {
+    pub fn as_raw(&self) -> *mut bindings::device {
         self.0.get()
     }
 
