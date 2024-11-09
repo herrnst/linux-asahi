c8088b1539f6f1d900e23c49f760ff1db759556e
Sasha Finkelstein <fnkl.kernel@gmail.com>
2024-11-09 18:27:59 +0100

    rust: alloc: kvec: WIP(?): Add swap_remove() for AOP series
    Signed-off-by: Sasha Finkelstein <fnkl.kernel@gmail.com>
    Signed-off-by: Janne Grunau <j@jannau.net>

--- rust/kernel/alloc/kvec.rs
+++ rust/kernel/alloc/kvec.rs
@@ -799,6 +799,26 @@ where
             }
         }
     }
+    /// Removes an element from the vector and returns it.
+    ///
+    /// The removed element is replaced by the last element of the vector.
+    ///
+    /// This does not preserve ordering of the remaining elements, but is *O*(1).
+    /// If you need to preserve the element order, use [`remove`] instead.
+    pub fn swap_remove(&mut self, index: usize) -> T {
+        if index > self.len() {
+            panic!("Index out of range");
+        }
+        // SAFETY: index is in range
+        // self.len() - 1 is in range since at last 1 element exists
+        unsafe {
+            let old = ptr::read(self.as_ptr().add(index));
+            let last = ptr::read(self.as_ptr().add(self.len() - 1));
+            ptr::write(self.as_mut_ptr().add(index), last);
+            self.dec_len(1);
+            old
+        }
+    }
 }
 
 impl<T: Clone, A: Allocator> Vec<T, A> {
