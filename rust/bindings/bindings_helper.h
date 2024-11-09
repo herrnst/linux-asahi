b640988b32003dc61a2a7d3110def3e79e3180e9
Andreas Hindborg <a.hindborg@samsung.com>
2024-11-09 18:25:16 +0100

    rust: add dma pool and coherent allocator
    Based on https://github.com/wedsonaf/linux/commit/02541e65a7e778c0049fed86ae49302bc07abed3

--- rust/bindings/bindings_helper.h
+++ rust/bindings/bindings_helper.h
@@ -18,6 +18,7 @@
 #include <linux/cred.h>
 #include <linux/delay.h>
 #include <linux/dma-mapping.h>
+#include <linux/dmapool.h>
 #include <linux/errname.h>
 #include <linux/ethtool.h>
 #include <linux/file.h>
@@ -33,6 +34,7 @@
 #include <linux/of.h>
 #include <linux/of_address.h>
 #include <linux/of_device.h>
+#include <linux/of_dma.h>
 #include <linux/pci.h>
 #include <linux/phy.h>
 #include <linux/pid_namespace.h>
