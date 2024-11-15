c28694d1db7dd1b04a1b71e15832f9e995cde963
Janne Grunau <j@jannau.net>
2024-11-15 23:12:35 +0100

    fixup! input: apple_z2: Add a driver for Apple Z2 touchscreens

--- drivers/input/touchscreen/apple_z2.c
+++ drivers/input/touchscreen/apple_z2.c
@@ -5,7 +5,6 @@
  * Copyright (C) The Asahi Linux Contributors
  */
 
-#include <asm/unaligned.h>
 #include <linux/delay.h>
 #include <linux/firmware.h>
 #include <linux/input.h>
@@ -14,6 +13,7 @@
 #include <linux/module.h>
 #include <linux/of.h>
 #include <linux/spi/spi.h>
+#include <linux/unaligned.h>
 
 #define APPLE_Z2_NUM_FINGERS_OFFSET      16
 #define APPLE_Z2_FINGERS_OFFSET          24
