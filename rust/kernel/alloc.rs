7e2eb5308eb136f414707cf9c9f9209844cf9d73
Asahi Lina <lina@asahilina.net>
2025-02-03 05:32:07 +0900

    rust: alloc: Flags: Switch to declare_flags_type!() macro.
    Signed-off-by: Asahi Lina <lina@asahilina.net>

--- rust/kernel/alloc.rs
+++ rust/kernel/alloc.rs
@@ -26,50 +26,20 @@ pub use self::kvec::KVec;
 pub use self::kvec::VVec;
 pub use self::kvec::Vec;
 
+use crate::types::declare_flags_type;
+
 /// Indicates an allocation error.
 #[derive(Copy, Clone, PartialEq, Eq, Debug)]
 pub struct AllocError;
 use core::{alloc::Layout, ptr::NonNull};
 
-/// Flags to be used when allocating memory.
-///
-/// They can be combined with the operators `|`, `&`, and `!`.
-///
-/// Values can be used from the [`flags`] module.
-#[derive(Clone, Copy, PartialEq)]
-pub struct Flags(u32);
-
-impl Flags {
-    /// Get the raw representation of this flag.
-    pub(crate) fn as_raw(self) -> u32 {
-        self.0
-    }
-
-    /// Check whether `flags` is contained in `self`.
-    pub fn contains(self, flags: Flags) -> bool {
-        (self & flags) == flags
-    }
-}
-
-impl core::ops::BitOr for Flags {
-    type Output = Self;
-    fn bitor(self, rhs: Self) -> Self::Output {
-        Self(self.0 | rhs.0)
-    }
-}
-
-impl core::ops::BitAnd for Flags {
-    type Output = Self;
-    fn bitand(self, rhs: Self) -> Self::Output {
-        Self(self.0 & rhs.0)
-    }
-}
-
-impl core::ops::Not for Flags {
-    type Output = Self;
-    fn not(self) -> Self::Output {
-        Self(!self.0)
-    }
+declare_flags_type! {
+    /// Flags to be used when allocating memory.
+    ///
+    /// They can be combined with the operators `|`, `&`, and `!`.
+    ///
+    /// Values can be used from the [`flags`] module.
+    pub struct Flags(u32);
 }
 
 /// Allocation flags.
