bf73061566209237f8254b01f8fb92b3644f6111
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2025-02-10 14:39:53 -0500

    iommu/dart: Track if the DART is locked
    Some DARTs are locked at boot-time. That means they are already
    configured and we cannot change their configuration, which requires
    special handling. Locked DARTs are identified in the configuration
    register. Check this bit when probing and save the result so we can
    handle accordingly.
    
    Signed-off-by: Alyssa Rosenzweig <alyssa@rosenzweig.io>
    Signed-off-by: Janne Grunau <j@jannau.net>

--- drivers/iommu/apple-dart.c
+++ drivers/iommu/apple-dart.c
@@ -203,6 +203,7 @@ struct apple_dart_hw {
  * @lock: lock for hardware operations involving this dart
  * @pgsize: pagesize supported by this DART
  * @supports_bypass: indicates if this DART supports bypass mode
+ * @locked: indicates if this DART is locked
  * @sid2group: maps stream ids to iommu_groups
  * @iommu: iommu core device
  */
@@ -224,6 +225,7 @@ struct apple_dart {
 	u32 num_streams;
 	u32 supports_bypass : 1;
 	u32 four_level : 1;
+	u32 locked : 1;
 
 	dma_addr_t dma_min;
 	dma_addr_t dma_max;
@@ -863,6 +865,8 @@ static int apple_dart_of_xlate(struct device *dev,
 			return -EINVAL;
 		if (cfg_dart->ias != dart->ias)
 			return -EINVAL;
+		if (cfg_dart->locked != dart->locked)
+			return -EINVAL;
 	}
 
 	cfg->supports_bypass &= dart->supports_bypass;
@@ -1164,6 +1168,11 @@ static irqreturn_t apple_dart_irq(int irq, void *dev)
 	return ret;
 }
 
+static bool apple_dart_is_locked(struct apple_dart *dart)
+{
+	return !!(readl(dart->regs + dart->hw->lock) & dart->hw->lock_bit);
+}
+
 static int apple_dart_probe(struct platform_device *pdev)
 {
 	int ret;
@@ -1262,6 +1271,7 @@ static int apple_dart_probe(struct platform_device *pdev)
 		goto err_clk_disable;
 	}
 
+	dart->locked = apple_dart_is_locked(dart);
 	ret = apple_dart_hw_reset(dart);
 	if (ret)
 		goto err_clk_disable;
@@ -1289,9 +1299,9 @@ static int apple_dart_probe(struct platform_device *pdev)
 
 	dev_info(
 		&pdev->dev,
-		"DART [pagesize %x, %d streams, bypass support: %d, bypass forced: %d, AS %d -> %d] initialized\n",
+		"DART [pagesize %x, %d streams, bypass support: %d, bypass forced: %d, locked: %d, AS %d -> %d] initialized\n",
 		dart->pgsize, dart->num_streams, dart->supports_bypass,
-		dart->pgsize > PAGE_SIZE, dart->ias, dart->oas);
+		dart->pgsize > PAGE_SIZE, dart->locked, dart->ias, dart->oas);
 	return 0;
 
 err_sysfs_remove:
