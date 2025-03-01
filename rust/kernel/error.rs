504f16e006e15d604cc28c64e63548b3261097f8
Janne Grunau <j@jannau.net>
2025-03-01 14:24:18 +0100

    rust: error: Add ENOSYS from uapi/asm-generic/errno.h
    Signed-off-by: Janne Grunau <j@jannau.net>

--- rust/kernel/error.rs
+++ rust/kernel/error.rs
@@ -66,6 +66,7 @@ pub mod code {
     declare_err!(EPIPE, "Broken pipe.");
     declare_err!(EDOM, "Math argument out of domain of func.");
     declare_err!(ERANGE, "Math result not representable.");
+    declare_err!(ENOSYS, "Invalid system call number.");
     declare_err!(ENODATA, "No data available.");
     declare_err!(EOVERFLOW, "Value too large for defined data type.");
     declare_err!(ETIMEDOUT, "Connection timed out.");
