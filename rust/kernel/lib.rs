c4fbff954e322e3b3e93e09c629396daf0df43ea
Lyude Paul <lyude@redhat.com>
2025-05-16 12:25:57 -0400

    rust: Add dma_buf stub bindings
    In order to implement the gem export callback, we need a type to represent
    struct dma_buf. So - this commit introduces a set of stub bindings for
    dma_buf. These bindings provide a ref-counted DmaBuf object, but don't
    currently implement any functionality for using the DmaBuf.
    
    Signed-off-by: Lyude Paul <lyude@redhat.com>

--- rust/kernel/lib.rs
+++ rust/kernel/lib.rs
@@ -100,6 +100,9 @@ pub mod device;
 pub mod device_id;
 pub mod devres;
 pub mod dma;
+pub mod dma_buf;
+#[cfg(CONFIG_DMA_SHARED_BUFFER)]
+pub mod dma_fence;
 pub mod driver;
 #[cfg(CONFIG_DRM = "y")]
 pub mod drm;
