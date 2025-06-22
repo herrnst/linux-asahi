a8c909a16a00af1f7004d58857fb47d91d0a0c85
Janne Grunau <j@jannau.net>
2025-06-22 11:52:29 +0200

    rust: device: Add support for locking the device
    Signed-off-by: Janne Grunau <j@jannau.net>

--- rust/kernel/device.rs
+++ rust/kernel/device.rs
@@ -7,7 +7,7 @@
 use crate::{
     bindings, fmt,
     sync::aref::ARef,
-    types::{ForeignOwnable, Opaque},
+    types::{ForeignOwnable, NotThreadSafe, Opaque},
 };
 use core::{marker::PhantomData, ptr};
 
@@ -398,6 +398,35 @@ impl<Ctx: DeviceContext> Device<Ctx> {
         // `struct fwnode_handle*` to a `*const FwNode` because `FwNode` is
         // defined as a `#[repr(transparent)]` wrapper around `fwnode_handle`.
         Some(unsafe { &*fwnode_handle.cast() })
+    }
+
+    /// Locks the [`Device`] for exclusive access.
+    pub fn lock(&self) -> Guard<'_, Ctx> {
+        // SAFETY: `self` is always valid by the type invariant.
+        unsafe { bindings::device_lock(self.as_raw()) };
+
+        Guard {
+            dev: self,
+            _not_send: NotThreadSafe,
+        }
+    }
+}
+
+/// A lock guard.
+///
+/// The lock is unlocked when the guard goes out of scope.
+#[must_use = "the lock unlocks immediately when the guard is unused"]
+pub struct Guard<'a, Ctx: DeviceContext = Normal> {
+    dev: &'a Device<Ctx>,
+    _not_send: NotThreadSafe,
+}
+
+impl<Ctx: DeviceContext> Drop for Guard<'_, Ctx> {
+    fn drop(&mut self) {
+        // SAFETY:
+        // - `self.xa.xa` is always valid by the type invariant.
+        // - The caller holds the lock, so it is safe to unlock it.
+        unsafe { bindings::device_unlock(self.dev.as_raw()) };
     }
 }
 
