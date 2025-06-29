9c0ee7221bc9dff9291c5377ba4aba3db987f2f5
Janne Grunau <j@jannau.net>
2025-06-29 09:49:53 +0200

    rust: drm: Move FEATURES back to drivers
    This can be used in an unsafe way but required for the asahi driver.
    
    Signed-off-by: Janne Grunau <j@jannau.net>

--- rust/kernel/drm/device.rs
+++ rust/kernel/drm/device.rs
@@ -86,7 +86,7 @@ impl<T: drm::Driver> Device<T> {
         name: crate::str::as_char_ptr_in_const_context(T::INFO.name).cast_mut(),
         desc: crate::str::as_char_ptr_in_const_context(T::INFO.desc).cast_mut(),
 
-        driver_features: drm::driver::FEAT_GEM,
+        driver_features: T::FEATURES,
         ioctls: T::IOCTLS.as_ptr(),
         num_ioctls: T::IOCTLS.len() as i32,
         fops: &Self::GEM_FOPS,
