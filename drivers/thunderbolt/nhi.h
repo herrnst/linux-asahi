706955fcfd837d3f2ed24949b40b2e8b31a981a8
Sven Peter <sven@kernel.org>
2025-08-25 14:49:13 +0000

    thunderbolt: Add ring_interrupt_mask to nhi_tb_ops
    Signed-off-by: Sven Peter <sven@kernel.org>

--- drivers/thunderbolt/nhi.h
+++ drivers/thunderbolt/nhi.h
@@ -49,6 +49,7 @@ struct tb_nhi_ops {
 	int (*ring_request_irq)(struct tb_ring *ring, bool no_suspend);
 	void (*ring_release_irq)(struct tb_ring *ring);
 	void (*ring_interrupt_active)(struct tb_ring *ring, bool active);
+	void (*ring_interrupt_mask)(struct tb_ring *ring, bool mask);
 	void __iomem *(*ring_desc_base)(struct tb_ring *ring);
 	void __iomem *(*ring_options_base)(struct tb_ring *ring);
 };
