fdb19feb784e4c2c6c2188d314318d46eef7dc67
Sven Peter <sven@kernel.org>
2025-08-25 14:45:53 +0000

    thunderbolt: Remove pci_dev from struct tb_nhi
    Prepare to support non-PCI thunderbolt NHIs by removing the pci_dev
    pointer from struct tb_nhi and replacing it with struct device pointer
    instead. Note that parts of the code still assume that the struct device
    is backed by a pci_device after this. That assumption will be fixed in the
    following commits for the Apple Silicon platform NHI.
    
    Signed-off-by: Sven Peter <sven@kernel.org>

--- drivers/thunderbolt/switch.c
+++ drivers/thunderbolt/switch.c
@@ -219,7 +219,7 @@ static void nvm_authenticate_start_dma_port(struct tb_switch *sw)
 	 * itself. To be on the safe side keep the root port in D0 during
 	 * the whole upgrade process.
 	 */
-	root_port = pcie_find_root_port(sw->tb->nhi->pdev);
+	root_port = pcie_find_root_port(to_pci_dev(sw->tb->nhi->dev));
 	if (root_port)
 		pm_runtime_get_noresume(&root_port->dev);
 }
@@ -228,7 +228,7 @@ static void nvm_authenticate_complete_dma_port(struct tb_switch *sw)
 {
 	struct pci_dev *root_port;
 
-	root_port = pcie_find_root_port(sw->tb->nhi->pdev);
+	root_port = pcie_find_root_port(to_pci_dev(sw->tb->nhi->dev));
 	if (root_port)
 		pm_runtime_put(&root_port->dev);
 }
