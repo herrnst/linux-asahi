8445999d4f39c3658d5e161a6514af36913f0285
Sven Peter <sven@kernel.org>
2025-08-25 14:44:29 +0000

    thunderbolt: Allow reading DROM from device tree
    On Apple Silicon SoCs the DROM is provided by a device tree property.
    Add support to read it from there as well.
    
    Signed-off-by: Sven Peter <sven@kernel.org>

--- drivers/thunderbolt/eeprom.c
+++ drivers/thunderbolt/eeprom.c
@@ -461,14 +461,14 @@ static void tb_switch_drom_free(struct tb_switch *sw)
 }
 
 /*
- * tb_drom_copy_efi - copy drom supplied by EFI to sw->drom if present
+ * tb_drom_copy_property - copy drom from a device property if present
  */
-static int tb_drom_copy_efi(struct tb_switch *sw, u16 *size)
+static int tb_drom_copy_property(struct tb_switch *sw, const char *name, u16 *size)
 {
 	struct device *dev = &sw->tb->nhi->pdev->dev;
 	int len, res;
 
-	len = device_property_count_u8(dev, "ThunderboltDROM");
+	len = device_property_count_u8(dev, name);
 	if (len < 0 || len < sizeof(struct tb_drom_header))
 		return -EINVAL;
 
@@ -476,8 +476,7 @@ static int tb_drom_copy_efi(struct tb_switch *sw, u16 *size)
 	if (res)
 		return res;
 
-	res = device_property_read_u8_array(dev, "ThunderboltDROM", sw->drom,
-									len);
+	res = device_property_read_u8_array(dev, name, sw->drom, len);
 	if (res)
 		goto err;
 
@@ -491,6 +490,22 @@ static int tb_drom_copy_efi(struct tb_switch *sw, u16 *size)
 err:
 	tb_switch_drom_free(sw);
 	return -EINVAL;
+}
+
+/*
+ * tb_drom_copy_of_apple - copy drom supplied by the device tree for Apple Silicon to sw->drom.
+ */
+static int tb_drom_copy_of_apple(struct tb_switch *sw, u16 *size)
+{
+	return tb_drom_copy_property(sw, "apple,thunderbolt-drom", size);
+}
+
+/*
+ * tb_drom_copy_efi - copy drom supplied by EFI to sw->drom if present
+ */
+static int tb_drom_copy_efi(struct tb_switch *sw, u16 *size)
+{
+	return tb_drom_copy_property(sw, "ThunderboltDROM", size);
 }
 
 static int tb_drom_copy_nvm(struct tb_switch *sw, u16 *size)
@@ -667,6 +682,10 @@ err:
 static int tb_drom_host_read(struct tb_switch *sw)
 {
 	u16 size;
+
+	/* Apple Silicon machines always get their DROM from the Device Tree */
+	if (!tb_drom_copy_of_apple(sw, &size))
+		return tb_drom_parse(sw, size);
 
 	if (tb_switch_is_usb4(sw)) {
 		usb4_switch_read_uid(sw, &sw->uid);
