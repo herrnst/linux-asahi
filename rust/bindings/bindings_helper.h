7636ce94cc4c5f17510c61a3ab935c25e89961c5
Asahi Lina <lina@asahilina.net>
2025-12-02 17:03:31 -0500

    rust: drm: gem: shmem: Add DRM shmem helper abstraction
    The DRM shmem helper includes common code useful for drivers which
    allocate GEM objects as anonymous shmem. Add a Rust abstraction for
    this. Drivers can choose the raw GEM implementation or the shmem layer,
    depending on their needs.
    
    Signed-off-by: Asahi Lina <lina@asahilina.net>
    Signed-off-by: Daniel Almeida <daniel.almeida@collabora.com>
    Signed-off-by: Lyude Paul <lyude@redhat.com>

--- rust/bindings/bindings_helper.h
+++ rust/bindings/bindings_helper.h
@@ -33,6 +33,7 @@
 #include <drm/drm_drv.h>
 #include <drm/drm_file.h>
 #include <drm/drm_gem.h>
+#include <drm/drm_gem_shmem_helper.h>
 #include <drm/drm_ioctl.h>
 #include <kunit/test.h>
 #include <linux/auxiliary_bus.h>
@@ -61,6 +62,7 @@
 #include <linux/fs.h>
 #include <linux/i2c.h>
 #include <linux/ioport.h>
+#include <linux/iosys-map.h>
 #include <linux/jiffies.h>
 #include <linux/jump_label.h>
 #include <linux/mdio.h>
