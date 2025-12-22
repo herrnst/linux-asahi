21c4df77950e8febe997a47e7f013e5ab4efb925
James Calligeros <jcalligeros99@gmail.com>
2025-12-22 19:30:47 +1000

    drm: apple: Support YCbCr formats
    Support 8-bit YCbCr planar and semi-planar formats in 4:2:0, 4:2:2 and
    4:4:4 sub-sampling with limited and full range.
    Use the signalled color space and transfer functions for YCbCr formats.
    DCP allows a unique colour space to be specified for each surface. The
    firmware then tonemaps this to the connected display's native colour
    space. KMS sets color_encoding and color_range only for YCbCr formats.
    
    Signed-off-by: James Calligeros <jcalligeros99@gmail.com>
    Co-developed-by: Janne Grunau <j@jannau.net>
    Signed-off-by: Janne Grunau <j@jannau.net>

--- drivers/gpu/drm/apple/iomfb.h
+++ drivers/gpu/drm/apple/iomfb.h
@@ -80,17 +80,6 @@ enum iomfb_property_id {
 /* We have 4 surfaces, but we can only ever blend two */
 #define MAX_BLEND_SURFACES 2
 
-enum dcp_colorspace {
-	DCP_COLORSPACE_BG_SRGB = 0,
-	DCP_COLORSPACE_BG_BT2020 = 9,
-	DCP_COLORSPACE_NATIVE = 12,
-};
-
-enum dcp_xfer_func {
-	DCP_XFER_FUNC_SDR = 13,
-	DCP_XFER_FUNC_HDR = 16,
-};
-
 struct dcp_iouserclient {
 	/* Handle for the IOUserClient. macOS sets this to a kernel VA. */
 	u64 handle;
