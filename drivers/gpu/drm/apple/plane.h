1465b5f098eb346da4eb62ecdbae6a31b453a7a1
Janne Grunau <j@jannau.net>
2025-12-24 13:12:11 +0100

    drm: apple: Move plane bits out of apple_drv/iomfb_flush
    Now that dcp may use multiple planes with more complex paramters move
    this out of iomfb_flush and use the appropriate KMS atomic plane helper
    functions.
    Also move most plane handling functions from apple_drv.c to its own
    file.
    
    Signed-off-by: Janne Grunau <j@jannau.net>

---
+++ drivers/gpu/drm/apple/plane.h
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-2.0-only OR MIT
+/*
+ * Copyright (C) The Asahi Linux Contributors
+ */
+
+#ifndef __APPLE_PLANE_H__
+#define __APPLE_PLANE_H__
+
+#include <drm/drm_plane.h>
+
+#include <linux/types.h>
+
+#include "iomfb_plane.h"
+
+struct apple_plane_state {
+	struct drm_plane_state base;
+	struct dcp_surface surf;
+};
+
+#define to_apple_plane_state(x) container_of(x, struct apple_plane_state, base)
+
+struct drm_plane *apple_plane_init(struct drm_device *dev,
+				   unsigned long possible_crtcs,
+				   enum drm_plane_type type);
+
+#endif /* __APPLE_PLANE_H__ */
