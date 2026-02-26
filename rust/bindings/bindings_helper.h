8c646eeb9cab468896d29b30b3ab77aeef29e5e7
Sasha Finkelstein <fnkl.kernel@gmail.com>
2026-02-26 17:52:13 +0100

    rust: bindings: WIP(?): Export various bits for PMP driver
    Signed-off-by: Sasha Finkelstein <fnkl.kernel@gmail.com>

--- rust/bindings/bindings_helper.h
+++ rust/bindings/bindings_helper.h
@@ -71,6 +71,7 @@
 #include <linux/iio/types.h>
 #include <linux/interrupt.h>
 #include <linux/io-pgtable.h>
+#include <linux/iommu.h>
 #include <linux/ioport.h>
 #include <linux/iosys-map.h>
 #include <linux/jiffies.h>
